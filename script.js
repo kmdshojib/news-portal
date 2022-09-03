68d8b695da089aec74571fe542667dbb7debdae8
kmdshojib <kmd.shojib123@gmail.com>
2022-09-03 15:27:54 +0600

    card added

--- script.js
+++ script.js
@@ -34,9 +34,47 @@ const displayNews = (id) =>{
     
 }
 
+// sorting by views and showing according to the views
+
 const showNews = (news) =>{
-    console.log(news.sort((a,b) => b.total_view - a.total_view))
+    // sorted object
+    const sortedObj = (news.sort((a,b) => b.total_view - a.total_view));
+    // getting  element from DOM
+    const newsSection = document.getElementById('news-section');
+    newsSection.textContent ='';
+    if(sortedObj.length > 0){
+        sortedObj.map( element =>{
+            console.log(element)
+            const newsItem = document.createElement("div");
+            newsItem.innerHTML = `
+            <div class="card mb-3" style="max-width: 740px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src=${element.image_url} class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                        <h5 class="card-title">${element.title}</hh5>
+                        <p class="card-text">${element.details}</p>
+                    </div>
+                </div>
+                <div class="d-flex flex-column">
+                        <img src=${element.author.img} class="avatar" alt="avatar">
+                        <div class="d-flex flex-row">
+                            <p class="card-text"><small class="text-muted">${element.author.name}</small></p>
+                            <p class="card-text"><small class="text-muted">${element.author.published_date}</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `
+            newsSection.appendChild(newsItem)
+        })
+        
+    }
 }
+
+// fetching the news
 const fetchNews = (newsCategory) =>{
     const  url = `https://openapi.programming-hero.com/api/news/category/${newsCategory}`
     fetch(url)
